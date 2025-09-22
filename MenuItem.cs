1013565f73b6cf57ec0bc3fc2b2fd8f76674e507
alexonplus <oleks9185@gmail.com>
2025-09-23 00:29:08 +0200

    created a base class with two fields 1(Name and Price) and one method (ShowInfo).  in class (MenuItem)
    The console will only show the name and price.
    
    The main branch  without OOP principles.

---
+++ MenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopGitHub_Branch_4princip
+{
+    public class MenuItem
+    {
+        public string Name;
+        public int Price;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} {Price} kr");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            MenuItem pizza = new MenuItem();
+            pizza.Name = "Pizza";
+            pizza.Price =  80;
+            pizza.ShowInfo();
+
+            MenuItem burger = new MenuItem();
+            burger.Name = "Big-Mac";
+            burger.Price = 55;
+            burger.ShowInfo();
         }
     }
 }

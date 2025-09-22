e4c7e49fb09e75183988738e1fd08c6e2aa0af19
alexonplus <oleks9185@gmail.com>
2025-09-22 23:22:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace OopGitHub_Branch_4princip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+/*Varje branch ska:
+
+ Ha egna commit-meddelanden
+ Ha tydliga kommentarer i koden
+ När du är klar – skapa en Pull Request till main                                                           
+ PR:n ska beskriva vilken princip du implementerat och vad du lärt dig
+             *             *                          *
+ Du ska alltså ha 4 olika Pull Requests – en för varje princip.
+
+*                            *                           *
+_______________________main.class________utan OOP-principer___________________
+Exempel på struktur:
+main
+BankAccount.cs(grundläggande klass utan OOP-principer)
+
+__________________________encapsulation_______________________________________
+Gör Balance privat, lägg till Deposit() och Withdraw()
+inheritance
+______________________________________________________________________________
+Lägg till SavingsAccount : BankAccount
+polymorphism
+_______________________________________________________________________________
+Skapa 1virtual och 2override metoder
+3abstraction
+Skapa 4abstract class Account och implementera den i underklasser */
+
+//()()()(R)()(E)()(S)()(T)()(O)()R)()()(A)()()(N)()()()()()()()()()()()()()()()()()
+
+/*____main.class________utan OOP-principer
+Exempel på struktur:
+main
+MenuItem.cs(grundläggande klass utan OOP-principer)
+
+________________________encapsulation_____________
+Gör Price privat, lägg till SetPrice(), IncreasePrice() och DecreasePrice()
+
+______________________________inheritance_______________
+Lägg till Drink : MenuItem
+
+_________________________polymorphism_____________
+Skapa 1 virtual metod i MenuItem och 1 override metod i Drink
+
+___________________________________abstraction_______________
+Skapa abstract class ItemBase och implementera ShowInfo() i MenuItem och Drink
+/*
+
